6113612445cb760eea23318c7503675090c02815
yxyuan <eshoyuan@gmail.com>
2023-05-09 10:38:20 +0000

    update multi-thread and face

---
+++ demo.sh
@@ -0,0 +1,5 @@
+python demoTalkNet_face+.py --videoName 001
+python demoTalkNet_face+.py --videoName 002
+python demoTalkNet_face+.py --videoName 003
+python demoTalkNet_face+.py --videoName 004
+python demoTalkNet_face+.py --videoName 005
